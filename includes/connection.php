31505f8340b234302b3a9479138ecf2c569bed11
Ishwar Baisla <ishwar2303@gmail.com>
2021-02-04 00:54:58 +0530

    v0.1

---
+++ includes/connection.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "zarurat";
+
+$con = new mysqli($servername,$username,$password,$dbname);
+    session_start();
+?>
